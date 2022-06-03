9870775bfc12b609e03a0298444b7173d3994831
Protik111 <rafiurprotik111@gmail.com>
2022-06-03 08:58:48 +0600

    class 44th form error hanlding added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BottomNav from '../components/BottomNav';
+import App from '../components/ErrorHandlerForm/App';
 import FetchDataByHook from '../components/FetchDataByHook/FetchDataByHook';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
@@ -9,7 +10,7 @@ import styles from '../styles/Home.module.css';
 const Home = () => {
     return (
         <div className={`${styles.home} container-fluid p-0`}>
-            <Navbar></Navbar>
+            {/* <Navbar></Navbar> */}
             {/* <div className="row">
                 <div className={`${styles.sidebar} col-md-2 col-sm-1 col-xs-0`}>
                     <Sidebar></Sidebar>
@@ -19,7 +20,8 @@ const Home = () => {
                 </div>
             </div>
             <BottomNav></BottomNav> */}
-            <FetchDataByHook></FetchDataByHook>
+            {/* <FetchDataByHook></FetchDataByHook> */}
+            <App></App>
         </div>
     );
 };
